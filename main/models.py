767bc9f3a8079e6362f91483a0ed4e4fd5de515f
Yesil <yesil@gmail.com>
2023-12-04 17:09:38 +0300

    feat: model and template

---
+++ main/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+# Create your models here.
+
+class Main(models.Model):
+    name=models.CharField(max_length=30)
+    about = models.TextField()
+    fb=models.CharField(max_length=30,default="")
+    tw=models.CharField(max_length=30,default="")
+    yt=models.CharField(max_length=30,default="")
+    set_name=models.CharField(max_length=30,default="")
+    tell=models.CharField(max_length=30,default="")
+    link=models.CharField(max_length=30,default="")
+
+
+
+
+
+    def __str__(self):
+        return self.set_name
